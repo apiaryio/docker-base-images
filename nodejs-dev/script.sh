a52e56d2df109ef315617d3f65ce3422a6a2ff96
Ladislav Prskavec <ladislav@prskavec.net>
2015-03-16 20:03:08 +0100

    Upgrade nodejs to 0.10.37

--- nodejs-dev/script.sh
+++ nodejs-dev/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/creationix/nvm/v0.23.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install v0.10.36
-nvm alias default v0.10.36
+nvm install v0.10.37
+nvm alias default v0.10.37
 npm -g install npm@1.4.28
 npm -g install coffee-script@1.8.0 grunt-cli@0.1.13
