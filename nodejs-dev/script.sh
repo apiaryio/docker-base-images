f13f5ebccba10135832e3dfa876932a6004f4408
Ladislav Prskavec <ladislav@prskavec.net>
2015-03-24 16:46:00 +0100

    Update npm to 2.7.3

--- nodejs-dev/script.sh
+++ nodejs-dev/script.sh
@@ -3,5 +3,5 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.23.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v0.10.37
 nvm alias default v0.10.37
-npm -g install npm@1.4.28
+npm -g install npm@2.7.3
 npm -g install coffee-script@1.8.0 grunt-cli@0.1.13
