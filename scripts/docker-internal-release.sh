d114c8669abd5479e0f83e656871984625ca9656
nadade <nada@apiary.io>
2016-03-03 12:50:12 +0100

    handle dockerhub login error

--- scripts/docker-internal-release.sh
+++ scripts/docker-internal-release.sh
@@ -9,7 +9,21 @@ if [[ -z "$DOCKER_EMAIL" || -z "$DOCKER_USER" || -z "$DOCKER_PASS" ]]; then
     echo "DockerHub credentials are missing (DOCKER_EMAIL, DOCKER_USER, DOCKER_PASS), cannot proceed."
     exit 1
 fi
+
+CREDENTIALS_FILE=~/.docker/config.json
+if [[ -e $CREDENTIALS_FILE && ! -w $CREDENTIALS_FILE ]]; then
+    echo "Found $CREDENTIALS_FILE but it's not writable, cannot proceed."
+    exit 1
+fi
+
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
+if [[ -e $CREDENTIALS_FILE ]]; then
+    if ! grep -q "index.docker.io" "$CREDENTIALS_FILE"; then
+        echo "Docker login failed, cannot proceed."
+        exit 1
+    fi
+fi
 
 for IMAGE_NAME in $IMAGES_TO_SQUASH
 do
