fe276f5e404d0e19c3b297754ebb0496fac851d4
Ladislav Prskavec <ladislav@apiary.io>
2016-08-04 14:25:16 +0200

    feat: add latest and stable tag to docker image

--- dredd/build.sh
+++ dredd/build.sh
@@ -1,3 +1,5 @@
 DREDD_VERSION=`./scripts/getLatestStableNodePackage dredd`
 echo "Building version: $DREDD_VERSION"
 docker build -t apiaryio/dredd:$DREDD_VERSION --build-arg DREDD_VERSION="$DREDD_VERSION" -f dredd/Dockerfile --no-cache dredd/
+docker tag apiaryio/dredd:$DREDD_VERSION apiaryio/dredd:stable
+docker tag apiaryio/dredd:$DREDD_VERSION apiaryio/dredd:latest
