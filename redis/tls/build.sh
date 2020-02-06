751503fd344c5a93db1d867a9fa49fbbcafebb6f
Jakub Korál <jakub.koral@oracle.com>
2020-02-06 12:38:50 +0100

    fix: use a specific version of ghostunnel

--- redis/tls/build.sh
+++ redis/tls/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -v "${PWD}"/redis/tls/:/go/bin apiaryio/golang go get -u github.com/square/ghostunnel
+docker run -v "${PWD}"/redis/tls/:/go/bin apiaryio/golang go get -u github.com/square/ghostunnel@v1.2.1
 
 docker build --no-cache -t apiaryio/redis:tls -f redis/tls/Dockerfile redis/tls/
