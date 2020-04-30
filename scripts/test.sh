f96c263792bf689135e468d51455b9af967f4769
Ladislav Prskavec <ladislav.prskavec@oracle.com>
2020-04-30 17:35:33 +0200

    feat: add circleci lint step

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-find . -name "Dockerfile" -print0 | xargs -0 -n1 dockerlint  -f $1
+#!/usr/bin/env bash
+set +o pipefail
+find . -name "Dockerfile" -print0 | xargs -0 -n1 dockerlint -f $1
+set -o pipefail
